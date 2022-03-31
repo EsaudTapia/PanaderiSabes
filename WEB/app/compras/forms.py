10370580db0065121cb5888b1826b0e9c64c48c5
EsaudTapia <19002144@alumnos.utleon.edu.mx>
2022-03-31 16:31:16 -0600

    compras campo proveedor

--- WEB/app/compras/forms.py
+++ WEB/app/compras/forms.py
@@ -7,26 +7,7 @@ from wtforms.validators import DataRequired
 class RegistroCompra(FlaskForm):  
     
     
-   apaterno=StringField('A. paterno', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=1, max=50, message='Ingresa un apellido valido')])
-   amaterno=StringField('A. materno', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=1, max=50, message='Ingresa un apellido valido')])
-   numero=StringField('Numero', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=1, max=50, message='Ingresa un grupo valido')])
-   calle=StringField('Calle', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=1, max=50, message='Ingresa una matricula valida')])
-   colonia=StringField('Colonia', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=1, max=50, message='Ingresa un dia valido')])
-   cp=IntegerField('C.P.', [validators.DataRequired(message='El campo es requerido')]
-                    )
-   telefono=StringField('Telefono', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=10, max=10, message='Ingresa un año valido')])
-   email=EmailField('Email', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=1, max=100, message='Ingresa un año valido')])
-   password=PasswordField('Contrseña', [validators.DataRequired(message='El campo es requerido'),
-                                      validators.length(min=1, max=100, message='Ingresa un año valido')])
-   
-   comboRol= SelectField('Escoja el Rol',choices=[])
+ 
    
    guardar = SubmitField("Guardar") 
    
--- WEB/app/compras/views.py
+++ WEB/app/compras/views.py
@@ -1,13 +1,19 @@
 from flask import render_template,session,redirect,flash,url_for
+from ..proveedores.models import Proveedor
+from ..insumos.models import Insumo
 from . import compras
 from .forms import RegistroCompra
 
 @compras.route("/Listado",methods=['GET','POST'])
 def listaco():
     formcom= RegistroCompra()
+    proveedores = Proveedor.query.all()
+    insumos = Insumo.query.all()
+   
     context = {
         'formcom':formcom,
-       
+        'proveedores': proveedores,
+        'insumos': insumos
      }
     
     return render_template("compras.html",**context)
