c86750f21584395965af73a7fe6dc7cf28853079
NazarethMoises10 <18002419@alumnos.utleon.edu.mx>
2022-04-02 00:12:55 -0600

    Ventas finalizado uwu

--- WEB/app/ventas/views.py
+++ WEB/app/ventas/views.py
@@ -27,11 +27,33 @@ def detalle(id):
 
 
 
-@ventas.route("/ventaactive",methods=['GET'])
-def ventaact():
-    ventas = Venta.query.join(User, Venta.id_cl==User.id).add_columns(Venta.status,Venta.id, User.nombre,Venta.fechaRegistro,Venta.total)
-    print(str(ventas))
-    context = {
-        'ventas': ventas
-    }
-    return render_template("ventas.html",**context)
+@ventas.route("/ventaactive/<id>",methods=['GET'])
+def ventaact(id):
+    ventas = Venta.query.join(DetalleVenta, DetalleVenta.id_venta==Venta.id).join(Producto, DetalleVenta.id_pr==Producto.id).add_columns(DetalleVenta.id_pr,DetalleVenta.cantidad).filter(Venta.id==id)
+
+    for i in ventas:
+        p = Producto.query.filter_by(id=i.id_pr).first()
+        p.cantidadPr = p.cantidadPr - i.cantidad
+        
+        if p.cantidadPr < 0:
+            flash("El producto {} no cuenta con la cantidad suficiente.".format(p.nombre), category="error")
+            return redirect(url_for('ventas.listav'))
+    
+    ven = Venta.query.filter_by(id=id).first()    
+    ven.status = "Realizada"
+    
+    db.session.commit()
+    flash("La venta se ha realizado.", category="correcto")
+    return redirect(url_for('ventas.listav'))
+
+@ventas.route("/ventadelete/<id>",methods=['GET'])
+def ventadel(id):
+    
+    venta = Venta.query.filter_by(id=id).first()
+    ventas = DetalleVenta.query.filter_by(id_venta=id).first()
+
+    db.session.delete(venta)
+    
+    db.session.commit()
+    flash("La venta se ha cancelado.", category="correcto")
+    return redirect(url_for('ventas.listav'))
